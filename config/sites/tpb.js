b5792faa6f18eca96a81d1cc6f72e24d8b0c781e
Nicolas Acquaviva <nicolas.acquaviva@globant.com>
2017-10-26 20:59:24 -0300

    Updated tpb site config.

--- config/sites/tpb.js
+++ config/sites/tpb.js
@@ -1,33 +1,39 @@
+const baseUrl = 'https://proxyproxyproxy.nl'
+
 module.exports = {
-    result: {
-        listItem: "tr:not(.header)",
-        data: {
-            name: "a.detLink",
-            size: {
-                selector: ".detDesc",
-                convert: x => {
-                    return x.match(/Size (.*),/)[1]
+    url: (search) => {
+        return `${baseUrl}/s/?q=${encodeURIComponent(search)}&page=0&orderby=99`
+    },
+    scrapeConfig: {
+        result: {
+            listItem: "tr:not(.header)",
+            data: {
+                name: "a.detLink",
+                size: {
+                    selector: ".detDesc",
+                    convert: x => {
+                        return x.match(/Size (.*),/)[1]
+                    }
+                },
+                seeders: {
+                    selector: "td",
+                    eq: 2
+                },
+                leechers: {
+                    selector: "td",
+                    eq: 3
+                },
+                magnetLink: {
+                    selector: "a",
+                    eq: 3,
+                    attr: "href"
+                },
+                link: {
+                    selector: "a.detLink",
+                    attr: "href",
+                    convert: x => `${baseUrl}${x}`
                 }
-            },
-            seeders: {
-                selector: "td",
-                eq: 2
-            },
-            leechers: {
-                selector: "td",
-                eq: 3
-            },
-            magnetLink: {
-                selector: "a",
-                eq: 3,
-                attr: "href"
-            },
-            link: {
-                selector: "a.detLink",
-                attr: "href",
-                convert: x => `https://proxyproxyproxy.nl${x}`
             }
         }
     }
 }
-
